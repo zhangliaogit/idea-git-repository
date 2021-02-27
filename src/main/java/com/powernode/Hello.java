9e237bde2ca495d08e9fe894414a764b70f1e7ed
zs <zs@163.com>
2021-02-27 11:38:06 +0800

    添加了一行代码

--- src/main/java/com/powernode/Hello.java
+++ src/main/java/com/powernode/Hello.java
@@ -3,5 +3,6 @@ package com.powernode;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello java");
     }
 }
